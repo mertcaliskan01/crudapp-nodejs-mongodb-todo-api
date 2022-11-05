53117728cbaf0f3a84fc545e3b1cfca4febbdd4d
Mert <mcaliskanmert>
2022-11-05 10:40:57 +0300

    Cors configuration

--- app.js
+++ app.js
@@ -3,10 +3,16 @@ require("dotenv").config();
 require("./config/database").connect();
 const express = require("express");
 const error = require("./middleware/error");
+const cors = require("cors");
 
 
 const app = express();
 
+var corsOptions = {
+    origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 
