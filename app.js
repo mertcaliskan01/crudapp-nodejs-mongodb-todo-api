178a79adf22d7a47109fa37c05fc49b7480a0e57
Mert <mcaliskanmert>
2022-11-07 17:08:51 +0300

    disabled cors options for deploy

--- app.js
+++ app.js
@@ -8,11 +8,7 @@ const cors = require("cors");
 
 const app = express();
 
-var corsOptions = {
-    origin: "http://localhost:8081"
-};
-
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 
 
