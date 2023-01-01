71e43162b712a605aa3caeeb2c0639ae7cf82e98
Mert <mcaliskanmert>
2023-01-01 23:50:58 +0300

    test

--- tests/auth.test.js
+++ tests/auth.test.js
@@ -1,7 +1,7 @@
 const request = require("supertest");
 const app = require("../app");
 
-jest.setTimeout(30000)
+jest.setTimeout(60000)
 
 beforeAll(done => {
   done()
@@ -28,8 +28,13 @@ describe("/login", () => {
     it("should respond with a 400 status code", async () => {
 
       const bodyData = [
-        { email: "username" },
+        { email: "mcaliskanmert@gmail.com" },
+        { email: "example@gmail.com" },
+        { email: "email" },
+        { email: "" },
         { password: "123456" },
+        { password: "mmmmmm" },
+        { password: "" },
         {}
       ]
       for (const body of bodyData) {
@@ -42,15 +47,56 @@ describe("/login", () => {
 });
 
 describe("/register", () => {
-  it("Successful Register", async () => {
-    const res = await request(app).post("/register").send({
-      first_name: "Test",
-      last_name: "Test",
-      email: "mcaliskanmert@gmail.com",
-      password: "123456",
+  describe("given a info", () => {
+    it("should respond with a 409 status code", async () => {
+      const res = await request(app).post("/register").send({
+        first_name: "Test",
+        last_name: "Test",
+        email: "mcaliskanmert@gmail.com",
+        password: "123456",
+      });
+      expect(res.statusCode).toBe(409);
     });
-    expect(res.statusCode).toBe(409);
+
   });
 
+  describe("when one of the fields is missing", () => {
+    it("should respond with a 400 status code", async () => {
 
+      const bodyData = [
+        {
+          last_name: "Test",
+          email: "mcaliskanmert@gmail.com",
+          password: "123456",
+        },
+        {
+          first_name: "Test",
+          email: "mcaliskanmert@gmail.com",
+          password: "123456",
+        },
+        {
+          first_name: "Test",
+          last_name: "Test",
+          password: "123456",
+        },
+        {
+          first_name: "Test",
+          last_name: "Test",
+          email: "mcaliskanmert@gmail.com",
+        },
+        {
+          first_name: "",
+          last_name: "",
+          email: "",
+          password: "",
+        },
+        {}
+      ]
+      for (const body of bodyData) {
+        const res = await request(app).post("/register").send(body);
+        expect(res.statusCode).toBe(400)
+      }
+    });
+
+  })
 });
